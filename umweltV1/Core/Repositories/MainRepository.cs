c115929e4ff39523aed7364c973a7d2b539d8faf
MehrCodeLand <mehrcodeland@gmail.com>
2023-04-20 04:26:52 +0430

    validation is Done now we need sweet alert handle

--- umweltV1/Core/Repositories/MainRepository.cs
+++ umweltV1/Core/Repositories/MainRepository.cs
@@ -1,4 +1,5 @@
-﻿using umweltV1.Core.Interfaces;
+﻿using System.Text.RegularExpressions;
+using umweltV1.Core.Interfaces;
 using umweltV1.Data.Models.Users;
 using umweltV1.Data.MyDbContext;
 using umweltV1.Data.ViewModels;
@@ -22,13 +23,17 @@ namespace umweltV1.Core.Repositories
         }
 
         /*
-         -100:NULL
+         * Validation Code
+        -100:NULL
         -120:Username is to short
         -130:Compare Password
         -140:Password To Short
         -150:Email is Null
         -50:Username Is Exist 
         -30:Email Is Exist
+        -60:Email Structure is Not Correct
+        -70:Username Has a inCorecct Structure
+        100:All  IS ok
          */
         private int ValidationSignUpData(SignUpUserVm signUpUser)
         {
@@ -38,12 +43,22 @@ namespace umweltV1.Core.Repositories
             else if(signUpUser.Password.Length < 8 ) { return -140; }
             else if(signUpUser.Email == null ) { return -150;  }
 
-            if(IsUsername(signUpUser.Username.ToLower()) == -50) { return -50; }
-            if(IsEmail(signUpUser.Email.ToLower()) == -50) { return -30; }
+            else if(ValidateEmailRegex(signUpUser.Email.ToLower()) == -60) { return -60; }
+            else if(IsEmail(signUpUser.Email.ToLower()) == -50) { return -30; }
+            else if(IsUsername(signUpUser.Username.ToLower()) == -50) { return -50; }
+            else if(signUpUser.Username.Any(char.IsDigit) == true ) { return -70; }
 
-
-            return 0;
+            return 100 ;
         }
+        private int ValidateEmailRegex(string email)
+        {
+            string regex = @"^[^@\s]+@[^@\s]+\.(com|net|org|gov)$";
+            var result = Regex.IsMatch(email, regex, RegexOptions.IgnoreCase);
+
+            if(result != true) { return -60; }
+            return 60;
+        }
+
         #endregion
 
         #region IsExist
